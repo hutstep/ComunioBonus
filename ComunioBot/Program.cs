f6fa00c3ed68618bae64a1365ed1c30ba60fce30
Stephan Hüttner <stephan.huettner@yourccc.com>
2020-02-19 10:32:46 +0100

    Add project files.

---
+++ ComunioBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComunioBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
